34355cada939db0115a399d873a270d12235862d
satheeshglobal <satheeshglobal@gmail.com>
2023-04-05 10:50:01 +0530

    added src pom target

---
+++ src/main/java/com/myown/main/Random.java
@@ -0,0 +1,8 @@
+package com.myown.main;
+import java.lang.Math;
+public class Random {
+    public static void main(String[] args) {
+        System.out.println("hi satheesh");
+        System.out.println("random number is :"+Math.random());
+    }
+}
